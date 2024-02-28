f8568f7e28b985da484e0cb9272f46a5af38886f
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-02-28 16:11:05 -0300

    refactor: add responsive menu

--- src/app/core/component/header/header.component.ts
+++ src/app/core/component/header/header.component.ts
@@ -1,13 +1,25 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+''
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
 export class HeaderComponent {
+  public isMobileView!: boolean;
 
+  constructor() {
+    this.isMobileView = window.innerWidth <= 768;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(_event: any) {
+    this.isMobileView = window.innerWidth <= 768;
+  }
 }
