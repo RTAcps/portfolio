1f67798aa01d5b892fc71afbe1ac94386cf72fd1
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2026-02-25 14:26:26 -0300

    refactor: adjust app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideTranslateService({
+      defaultLanguage: 'en'
+    }),
+    ...provideTranslateHttpLoader({
+      prefix: '/assets/i18n/',
+      suffix: '.json'
+    }),
+  ],
 };
