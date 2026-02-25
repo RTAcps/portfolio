e0469d909cc9c853beb184d1273df99cee087172
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2026-02-25 11:20:31 -0300

    feat: create a projects section

---
+++ src/app/features/home/components/projects-section/projects-section.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, ExternalLink } from 'lucide-angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { mockData } from '../../../../shared/data/mock';
+import { Project } from '../../../../shared/models/projects';
+
+@Component({
+  selector: 'app-projects-section',
+  standalone: true,
+  imports: [CommonModule, LucideAngularModule, TranslateModule],
+  templateUrl: './projects-section.component.html',
+})
+export class ProjectsSectionComponent {
+  readonly ExternalLink = ExternalLink;
+
+  projects: Project[] = mockData.projects;
+  hoveredProject: number | null = null;
+
+  openLink(url: string): void {
+    window.open(url, '_blank');
+  }
+}
