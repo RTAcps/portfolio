563b4a357aade3755ad7ed146b3bd28b6bce1b31
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2026-02-25 11:19:27 -0300

    feat: create a experience section

---
+++ src/app/features/home/components/experience-section/experience-section.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, Briefcase, Calendar } from 'lucide-angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { mockData } from '../../../../shared/data/mock';
+import { Experience } from '../../../../shared/models/projects';
+
+@Component({
+  selector: 'app-experience-section',
+  standalone: true,
+  imports: [CommonModule, LucideAngularModule, TranslateModule],
+  templateUrl: './experience-section.component.html',
+})
+export class ExperienceSectionComponent {
+  readonly Briefcase = Briefcase;
+  readonly Calendar = Calendar;
+
+  experiences: Experience[] = mockData.experiences;
+}
